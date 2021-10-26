938e4438ebd77db92a3c3942727f38203c8f3faa
Farid-66 <70914203+Farid-66@users.noreply.github.com>
2021-10-26 22:32:55 +0300

    Tasks

---
+++ Tasks26/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Tasks26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+
+            /*Console.WriteLine(SumRange(5,9));
+
+            static int SumRange(int a, int b )
+            {
+                int sumRange = 0;
+
+                for (int i = a; i <= b; i++)
+                {
+                    sumRange= sumRange + i;
+                }
+                return sumRange;
+            
+            }*/
+
+            #endregion
+
+
+            #region Task2
+
+            /*Console.Write("First number:");
+            int firstnum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second number:");
+            int secondnum = Convert.ToInt32(Console.ReadLine());
+
+            int result = Sum(firstnum, secondnum);
+            Console.WriteLine($"{firstnum}+{secondnum}={result}");
+
+            static int Sum(int a, int b )
+            {
+                int sum = 0;
+
+                return sum=a+b;
+            
+            }*/
+
+            #endregion
+
+
+            #region Task3
+
+            /*Console.WriteLine(-1 + 4 * 6);
+            Console.WriteLine((35+5) % 7);
+            Console.WriteLine(14+(-4)*6/11);
+            Console.WriteLine(2+15/6*1-7%2);*/
+
+            #endregion
+
+        }
+
+    }
+}
